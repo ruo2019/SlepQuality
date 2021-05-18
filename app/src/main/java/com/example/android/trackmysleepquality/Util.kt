f0b6ca21ec0fe845cbea6530475e1691fc46941e
ruo20 <ruo2019@outlook.com>
2021-05-17 18:50:08 -0700

    no more errors!!!!

--- app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2019, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.trackmysleepquality
 
 import android.annotation.SuppressLint
@@ -24,6 +8,7 @@ import android.text.Spanned
 import androidx.core.text.HtmlCompat
 import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * These functions create a formatted string that can be set in a TextView.
